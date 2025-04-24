24138688a7d03150acc77ddd4b98bd2e59c5c1a8
Redstone2337 <3356168312@qq.com>
2025-04-24 09:37:34 +0800

    Update files

--- src/main/java/net/deepseek/v1/chainmining/data/ModModelsProvider.java
+++ src/main/java/net/deepseek/v1/chainmining/data/ModModelsProvider.java
@@ -34,14 +34,14 @@ public class ModModelsProvider extends FabricModelProvider {
 //            VariantSetting<Boolean> uvlock = VariantSettings.UVLOCK;
 //            VariantSetting<VariantSettings.Rotation> yRot = VariantSettings.Y;
 //            return VariantsBlockStateSupplier.create(vertBlock).coordinate(BlockStateVariantMap.create(TeleportBlock.FACING,TeleportBlock.SINGLE)
-//                    .register(Direction.NORTH, true, BlockStateVariant.create().put(VariantSettings.MODEL, vertId).put(uvlock, true))
-//                    .register(Direction.EAST, true, BlockStateVariant.create().put(VariantSettings.MODEL, vertId).put(uvlock, true).put(yRot, VariantSettings.Rotation.R90))
-//                    .register(Direction.SOUTH, true, BlockStateVariant.create().put(VariantSettings.MODEL, vertId).put(uvlock, true).put(yRot, VariantSettings.Rotation.R180))
-//                    .register(Direction.WEST, true, BlockStateVariant.create().put(VariantSettings.MODEL, vertId).put(uvlock, true).put(yRot, VariantSettings.Rotation.R270))
-//                    .register(Direction.NORTH, false, BlockStateVariant.create().put(VariantSettings.MODEL, fullBlockId).put(uvlock, true))
-//                    .register(Direction.EAST, false, BlockStateVariant.create().put(VariantSettings.MODEL, fullBlockId).put(uvlock, true))
-//                    .register(Direction.SOUTH, false, BlockStateVariant.create().put(VariantSettings.MODEL, fullBlockId).put(uvlock, true))
-//                    .register(Direction.WEST, false, BlockStateVariant.create().put(VariantSettings.MODEL, fullBlockId).put(uvlock, true)));
+//                    .register(Direction.NORTH, false, BlockStateVariant.create().put(VariantSettings.MODEL, vertId).put(uvlock, false))
+//                    .register(Direction.EAST, false, BlockStateVariant.create().put(VariantSettings.MODEL, vertId).put(uvlock, false).put(yRot, VariantSettings.Rotation.R90))
+//                    .register(Direction.SOUTH, false, BlockStateVariant.create().put(VariantSettings.MODEL, vertId).put(uvlock, false).put(yRot, VariantSettings.Rotation.R180))
+//                    .register(Direction.WEST, false, BlockStateVariant.create().put(VariantSettings.MODEL, vertId).put(uvlock, false).put(yRot, VariantSettings.Rotation.R270))
+//                    .register(Direction.NORTH, false, BlockStateVariant.create().put(VariantSettings.MODEL, fullBlockId).put(uvlock, false))
+//                    .register(Direction.EAST, false, BlockStateVariant.create().put(VariantSettings.MODEL, fullBlockId).put(uvlock, false))
+//                    .register(Direction.SOUTH, false, BlockStateVariant.create().put(VariantSettings.MODEL, fullBlockId).put(uvlock, false))
+//                    .register(Direction.WEST, false, BlockStateVariant.create().put(VariantSettings.MODEL, fullBlockId).put(uvlock, false)));
 //        }
 //    }
 
@@ -60,18 +60,18 @@ public class ModModelsProvider extends FabricModelProvider {
         itemModelGenerator.register(ModItems.BEDROCKIUM_PICKAXE, Models.HANDHELD);
 //        itemModelGenerator.register(ModItems.BEDROCKIUM_ARROW,Models.GENERATED);
 
-        itemModelGenerator.registerArmor(ModItems.BEDROCK_HELMET, ModEquipmentAssetKeys.BEDROCK_ASSET,"bedrock_helmet",true);
-        itemModelGenerator.registerArmor(ModItems.BEDROCK_CHESTPLATE, ModEquipmentAssetKeys.BEDROCK_ASSET,"bedrock_chestplate",true);
-        itemModelGenerator.registerArmor(ModItems.BEDROCK_LEGGINGS, ModEquipmentAssetKeys.BEDROCK_ASSET,"bedrock_leggings",true);
-        itemModelGenerator.registerArmor(ModItems.BEDROCK_BOOTS, ModEquipmentAssetKeys.BEDROCK_ASSET,"bedrock_boots",true);
+        itemModelGenerator.registerArmor(ModItems.BEDROCK_HELMET, ModEquipmentAssetKeys.BEDROCK_ASSET,"bedrock_helmet",false);
+        itemModelGenerator.registerArmor(ModItems.BEDROCK_CHESTPLATE, ModEquipmentAssetKeys.BEDROCK_ASSET,"bedrock_chestplate",false);
+        itemModelGenerator.registerArmor(ModItems.BEDROCK_LEGGINGS, ModEquipmentAssetKeys.BEDROCK_ASSET,"bedrock_leggings",false);
+        itemModelGenerator.registerArmor(ModItems.BEDROCK_BOOTS, ModEquipmentAssetKeys.BEDROCK_ASSET,"bedrock_boots",false);
 
-        itemModelGenerator.registerArmor(ModItems.BEDROCKIUM_HELMET, ModEquipmentAssetKeys.BEDROCKIUM_ASSET,"bedrockium_helmet",true);
-        itemModelGenerator.registerArmor(ModItems.BEDROCKIUM_CHESTPLATE, ModEquipmentAssetKeys.BEDROCKIUM_ASSET,"bedrockium_chestplate",true);
-        itemModelGenerator.registerArmor(ModItems.BEDROCKIUM_LEGGINGS, ModEquipmentAssetKeys.BEDROCKIUM_ASSET,"bedrockium_leggings",true);
-        itemModelGenerator.registerArmor(ModItems.BEDROCKIUM_BOOTS, ModEquipmentAssetKeys.BEDROCKIUM_ASSET,"bedrockium_boots",true);
+        itemModelGenerator.registerArmor(ModItems.BEDROCKIUM_HELMET, ModEquipmentAssetKeys.BEDROCKIUM_ASSET,"bedrockium_helmet",false);
+        itemModelGenerator.registerArmor(ModItems.BEDROCKIUM_CHESTPLATE, ModEquipmentAssetKeys.BEDROCKIUM_ASSET,"bedrockium_chestplate",false);
+        itemModelGenerator.registerArmor(ModItems.BEDROCKIUM_LEGGINGS, ModEquipmentAssetKeys.BEDROCKIUM_ASSET,"bedrockium_leggings",false);
+        itemModelGenerator.registerArmor(ModItems.BEDROCKIUM_BOOTS, ModEquipmentAssetKeys.BEDROCKIUM_ASSET,"bedrockium_boots",false);
 
         itemModelGenerator.registerBow(ModItems.BEDROCKIUM_BOW);
-        itemModelGenerator.registerArmor(ModItems.BEDROCKIUM_ARROW, ModEquipmentAssetKeys.BEDROCKIUM_ARROW,"bedrockium_arrow",true);
+        itemModelGenerator.registerArmor(ModItems.BEDROCKIUM_ARROW, ModEquipmentAssetKeys.BEDROCKIUM_ARROW,"bedrockium_arrow",false);
         itemModelGenerator.registerTippedArrow(ModItems.TRPPED_BEDROCKIUM_ARROW);
     }
 }
