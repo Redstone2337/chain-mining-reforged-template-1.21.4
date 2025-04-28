675b0f0e4bc00fe0a42a434f029a6b80afacc543
Redstone233 <3929326972@@qq.com>
2025-04-29 00:08:18 +0800

    Update files

--- src/main/java/net/deepseek/v1/chainmining/items/ModItems.java
+++ src/main/java/net/deepseek/v1/chainmining/items/ModItems.java
@@ -21,92 +21,92 @@ public class ModItems {
     public static final Item BEDROCK_SWORD = register(
             "bedrock_sword",
             (settings) -> new SwordItem(ModToolMaterial.BEDROCK,20,2.5F,
-                    settings.maxDamage(700000).rarity(Rarity.EPIC)
+                    settings.maxDamage(700000)
             ));
 
     public static final Item BEDROCK_PICKAXE = register(
             "bedrock_pickaxe",
             (settings) -> new PickaxeItem(ModToolMaterial.BEDROCK,15,1.5F,
-                    settings.maxDamage(6999000).rarity(Rarity.EPIC)
+                    settings.maxDamage(6999000)
             ));
 
     public static final Item BEDROCK_AXE = register(
             "bedrock_axe",
             (settings) -> new AxeItem(ModToolMaterial.BEDROCK,25,4.5F,
-                    settings.maxDamage(700200).rarity(Rarity.EPIC)
+                    settings.maxDamage(700200)
             ));
 
     public static final Item BEDROCK_SHOVEL = register(
             "bedrock_shovel",
             (settings) -> new ShovelItem(ModToolMaterial.BEDROCK,10,3.0F,
-                    settings.maxDamage(675000).rarity(Rarity.EPIC)
+                    settings.maxDamage(675000)
             ));
 
     public static final Item BEDROCK_HOE = register(
             "bedrock_hoe",
             (settings) -> new HoeItem(ModToolMaterial.BEDROCK,10,3.5F,
-                    settings.maxDamage(670000).rarity(Rarity.EPIC)
+                    settings.maxDamage(670000)
             ));
 
     public static final Item BEDROCKIUM_SWORD = register(
             "bedrockium_sword",
             (settings) -> new SwordItem(ModToolMaterial.BEDROCKIUM,20,2.5F,
-                    settings.maxDamage(710000).rarity(Rarity.EPIC)
+                    settings.maxDamage(710000)
             ));
 
     public static final Item BEDROCKIUM_PICKAXE = register(
             "bedrockium_pickaxe",
             (settings) -> new PickaxeItem(ModToolMaterial.BEDROCKIUM,15,1.5F,
-                    settings.maxDamage(7001000).rarity(Rarity.EPIC)
+                    settings.maxDamage(7001000)
             ));
 
     public static final Item BEDROCKIUM_AXE = register(
             "bedrockium_axe",
             (settings) -> new AxeItem(ModToolMaterial.BEDROCKIUM,25,4.5F,
-                    settings.maxDamage(710200).rarity(Rarity.EPIC)
+                    settings.maxDamage(710200)
             ));
 
     public static final Item BEDROCKIUM_SHOVEL = register(
             "bedrockium_shovel",
             (settings) -> new ShovelItem(ModToolMaterial.BEDROCKIUM,10,3.0F,
-                    settings.maxDamage(685000).rarity(Rarity.EPIC)
+                    settings.maxDamage(685000)
             ));
 
     public static final Item BEDROCKIUM_HOE = register(
             "bedrockium_hoe",
             (settings) -> new HoeItem(ModToolMaterial.BEDROCKIUM,10,3.5F,
-                    settings.maxDamage(680000).rarity(Rarity.EPIC)
+                    settings.maxDamage(680000)
             ));
 
     public static final Item BEDROCK_STONE = register("bedrock_stone",Item::new,
-            new Item.Settings().maxCount(64).rarity(Rarity.EPIC));
+            new Item.Settings().maxCount(64));
 
     public static final Item BEDROCKIUM_STONE = register("bedrockium_stone",Item::new,
-            new Item.Settings().maxCount(64).rarity(Rarity.EPIC));
+            new Item.Settings().maxCount(64));
 
     public static final Item DIAMOND_STICK = register("diamond_stick",Item::new,
-            new Item.Settings().maxCount(64).rarity(Rarity.EPIC));
+            new Item.Settings().maxCount(64));
 
     public static final Item BEDROCKIUM_ARROW = register("bedrockium_arrow",
-            (settings) -> new BedrockiumArrowItem(settings.maxCount(64).rarity(Rarity.EPIC)
+            (settings) -> new BedrockiumArrowItem(settings.maxCount(64)
                     .component(DataComponentTypes.TOOL,BedrockiumArrowItem.createToolComponent())
                     .attributeModifiers(BedrockiumArrowItem.createAttributeModifiers())
             ));
 
     public static final Item ICE_CLAW = register("ice_claw",
-            (settings) -> new IceClawItem(settings.maxCount(1).maxDamage(800000).rarity(Rarity.EPIC)
+            (settings) -> new IceClawItem(settings.maxCount(1).maxDamage(800000)
                     .component(DataComponentTypes.TOOL, IceClawItem.createToolComponent())
                     .attributeModifiers(IceClawItem.createAttributeModifiers())
             ));
 
     public static final Item TRPPED_BEDROCKIUM_ARROW = register("trpped_bedrockium_arrow",
-            (settings) -> new BedrockiumArrowItem(settings.maxCount(64).rarity(Rarity.EPIC)
+            (settings) -> new BedrockiumArrowItem(settings.maxCount(64)
                     .component(DataComponentTypes.TOOL,BedrockiumArrowItem.createToolComponent())
                     .attributeModifiers(BedrockiumArrowItem.createAttributeModifiers())
             ));
 
     public static final Item BEDROCKIUM_BOW = register("bedrockium_bow",
-            (settings) -> new BedrockiumBowItem(settings.maxCount(1).rarity(Rarity.EPIC)
+            (settings) -> new BedrockiumBowItem(settings.maxCount(1)
                     .maxDamage(8000000)
                     .component(DataComponentTypes.TOOL,BedrockiumBowItem.createToolComponent())
                     .attributeModifiers(BedrockiumBowItem.createAttributeModifiers())
@@ -114,35 +114,35 @@ public class ModItems {
 
     public static final Item BEDROCK_HELMET = register("bedrock_helmet",
             (settings) -> new ArmorItem(ModArmorMaterials.BEDROCK, EquipmentType.HELMET,
-                    settings.maxCount(1).rarity(Rarity.EPIC).fireproof()));
+                    settings.maxCount(1).fireproof()));
 
     public static final Item BEDROCK_CHESTPLATE = register("bedrock_chestplate",
             (settings) -> new ArmorItem(ModArmorMaterials.BEDROCK, EquipmentType.CHESTPLATE,
-                    settings.maxCount(1).rarity(Rarity.EPIC).fireproof()));
+                    settings.maxCount(1).fireproof()));
 
     public static final Item BEDROCK_LEGGINGS = register("bedrock_leggings",
             (settings) -> new ArmorItem(ModArmorMaterials.BEDROCK, EquipmentType.LEGGINGS,
-                    settings.maxCount(1).rarity(Rarity.EPIC).fireproof()));
+                    settings.maxCount(1).fireproof()));
 
     public static final Item BEDROCK_BOOTS = register("bedrock_boots",
             (settings) -> new ArmorItem(ModArmorMaterials.BEDROCK, EquipmentType.BOOTS,
-                    settings.maxCount(1).rarity(Rarity.EPIC).fireproof()));
+                    settings.maxCount(1).fireproof()));
 
     public static final Item BEDROCKIUM_HELMET = register("bedrockium_helmet",
             (settings) -> new ArmorItem(ModArmorMaterials.BEDROCKIUM, EquipmentType.HELMET,
-                    settings.maxCount(1).rarity(Rarity.EPIC).fireproof()));
+                    settings.maxCount(1).fireproof()));
 
     public static final Item BEDROCKIUM_CHESTPLATE = register("bedrockium_chestplate",
             (settings) -> new ArmorItem(ModArmorMaterials.BEDROCKIUM, EquipmentType.CHESTPLATE,
-                    settings.maxCount(1).rarity(Rarity.EPIC).fireproof()));
+                    settings.maxCount(1).fireproof()));
 
     public static final Item BEDROCKIUM_LEGGINGS = register("bedrockium_leggings",
             (settings) -> new ArmorItem(ModArmorMaterials.BEDROCKIUM, EquipmentType.LEGGINGS,
-                    settings.maxCount(1).rarity(Rarity.EPIC).fireproof()));
+                    settings.maxCount(1).fireproof()));
 
     public static final Item BEDROCKIUM_BOOTS = register("bedrockium_boots",
             (settings) -> new ArmorItem(ModArmorMaterials.BEDROCKIUM, EquipmentType.BOOTS,
-                    settings.maxCount(1).rarity(Rarity.EPIC).fireproof()));
+                    settings.maxCount(1).fireproof()));
 
 
 
